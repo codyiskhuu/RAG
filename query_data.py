c2134b3dd3e5e88182a0248960f5309f0d180dbf
codyiskhuu <khuucody@gmail.com>
2025-07-15 20:06:03 -0700

    Added comments for clarity

--- query_data.py
+++ query_data.py
@@ -33,20 +33,21 @@ def query_rag(query_text: str):
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Search the DB.
+    # Search the DB and pulls k lines of context -> document variable: ID, metadata, page_content.
     results = db.similarity_search_with_score(query_text, k=5)
 
-    
-
-
+    # Prepare prompt - Context Text = results but seperated.
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    # Pull the prompt tempalte + add context + question.
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
 
+    # Pull model + ask model prompt.
     model = OllamaLLM(model="mistral")
     response_text = model.invoke(prompt)
 
+    # Prepare formatted response.
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
